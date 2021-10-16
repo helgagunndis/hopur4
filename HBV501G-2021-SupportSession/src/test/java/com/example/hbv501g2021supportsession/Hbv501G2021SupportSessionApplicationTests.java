6946b45797b5d0f5d70d2fdbe09b2dbe9940c708
EsterJenny <ejb7@hi.is>
2021-10-16 18:55:34 +0000

    config changes

---
+++ HBV501G-2021-SupportSession/src/test/java/com/example/hbv501g2021supportsession/Hbv501G2021SupportSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hbv501g2021supportsession;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hbv501G2021SupportSessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
