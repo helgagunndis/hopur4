a2a6fd2e7104e902f52bfba78b166069e0887820
Helga <helgagunndis@gmail.com>
2021-10-22 21:14:42 +0000

    smá komið í user controller og service

--- HBV501G-2021-SupportSession/src/main/java/is/hi/hbv501g2021supportsession/Services/UserService.java
+++ HBV501G-2021-SupportSession/src/main/java/is/hi/hbv501g2021supportsession/Services/UserService.java
@@ -1,4 +1,9 @@
 package is.hi.hbv501g2021supportsession.Services;
+import is.hi.hbv501g2021supportsession.Persistence.Entities.User;
 
-public interface UserService {
+public interface UserService{
+    User save(User user);
+    User findByUsername(String userName);
+    User login(User user);
 }
+
