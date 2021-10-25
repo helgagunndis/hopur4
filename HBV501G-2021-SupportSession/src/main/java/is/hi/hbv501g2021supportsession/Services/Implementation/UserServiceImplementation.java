e71923e9702ea04436abfac6ef7db577b5d8fdfb
emh33 <55838653+emh33@users.noreply.github.com>
2021-10-25 00:12:43 +0000

    HTML signin
    Eftir að tengja við database

--- HBV501G-2021-SupportSession/src/main/java/is/hi/hbv501g2021supportsession/Services/Implementation/UserServiceImplementation.java
+++ HBV501G-2021-SupportSession/src/main/java/is/hi/hbv501g2021supportsession/Services/Implementation/UserServiceImplementation.java
@@ -1,6 +1,43 @@
 package is.hi.hbv501g2021supportsession.Services.Implementation;
 
+import is.hi.hbv501g2021supportsession.Persistence.Entities.User;
+import is.hi.hbv501g2021supportsession.Persistence.Repositories.UserRepository;
 import is.hi.hbv501g2021supportsession.Services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public class UserServiceImplementation implements UserService {
+    UserRepository userRepository;
+    
+    @Autowired
+    private UserServiceImplementation (UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
+    @Override
+    public void delete(User user) {
+         userRepository.delete(user);
+    }
+
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public User login(User user) {
+        return null;
+    }
 }
